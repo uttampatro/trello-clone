d82bae1b35fed69f9f869c9438f57a097bc54c14
uttampatro <uttampatro890@gmail.com>
2025-01-04 11:47:38 +0530

    add: Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>Trello Clone</h1>
+      <button>Reset Board</button>
+    </header>
+  );
+}
+
+export default Header;
