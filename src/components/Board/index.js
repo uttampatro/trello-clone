b2db81fb38eed12766e39afdda1b3d2703353b29
uttampatro <uttampatro890@gmail.com>
2025-01-04 13:13:27 +0530

    add: board

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Header from "../Header";
+import List from "../List";
 
-function Board() {
+const Board = () => {
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    const savedLists = JSON.parse(localStorage.getItem("lists")) || [];
+    setLists(savedLists);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(lists));
+  }, [lists]);
+
+  const resetBoard = () => {
+    setLists([]);
+  };
+
+  const addList = () => {
+    const title = prompt("Enter list title");
+    if (title) {
+      setLists([...lists, { id: Date.now(), title, cards: [] }]);
+    }
+  };
+
   return (
-    <div>Board</div>
-  )
-}
+    <div>
+      <Header resetBoard={resetBoard} />
+      <div className="flex overflow-x-auto py-6 space-x-4">
+        {lists.map((list) => (
+          <List key={list.id} list={list} setLists={setLists} />
+        ))}
+      </div>
+      <button
+        onClick={addList}
+        className="bg-green-500 text-white py-4 px-8 rounded"
+      >
+        Add New List
+      </button>
+    </div>
+  );
+};
 
-export default Board
+export default Board;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-function Header() {
+const Header = ({ resetBoard }) => {
   return (
     <header className="bg-[#0079bf] text-white p-4 flex justify-between items-center">
       <h1 className="text-xl">Trello Clone</h1>
-      <button className="bg-[#ff5c5c] text-white py-2 px-4 rounded hover:bg-[#e04d4d]">
+      <button
+        onClick={resetBoard}
+        className="bg-[#ff5c5c] text-white py-2 px-4 rounded hover:bg-[#e04d4d]"
+      >
         Reset Board
       </button>
     </header>
   );
-}
+};
 
 export default Header;
